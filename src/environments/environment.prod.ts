30e9a2408f435070a4123c5c85fc28a2c7f299de
ttmeron <meronteklehaimanot@gmail.com>
2026-03-16 16:52:13 -0700

    Add missing properties to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ethiopian-kitchen-backend-6.onrender.com/api'
+  apiUrl: 'https://ethiopian-kitchen-backend-6.onrender.com/api',stripePublishableKey: 'pk_test_51RTp6RReFIfKqq3ECN2HgFUL1isW4PRJzGxPfHp1T6Tkb0iI6kdLbkthj37gcynkU9L5Wh9pjt7JBoRKJ4TopC4K00EpYsHdeX',
+  orderApi: 'https://ethiopian-kitchen-backend-6.onrender.com/api/orders',
+  authApi: 'https://ethiopian-kitchen-backend-6.onrender.com/api/auth'
 };
